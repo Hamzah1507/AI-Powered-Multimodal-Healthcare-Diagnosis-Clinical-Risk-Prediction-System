bc5cd18c8576ab9a34a3fa6e34fcd0fbd1e2c7f9
Hamzah Saiyed <hamzah.2004saiyed@gmail.com>
2026-02-24 03:06:46 +0530

    feat: App updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 import Welcome from './welcome'
-import Auth from './Auth'
+import Auth from './auth'
 
 const API = 'http://127.0.0.1:8000'
 
